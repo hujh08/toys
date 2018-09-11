2925bcca1047aa2a803ba0552f3f7719294c7dfb
Jh Hu <hu.jianhong_2008@163.com>
2018-09-11 17:43:12 +0800

    remove useless comments

--- sudoku/readtxt.c
+++ sudoku/readtxt.c
@@ -35,28 +35,3 @@ void readtxt(char *filename, matrix *mat) {
         }
     }
 }
-
-// void readtxt(char *filename, matrix *mat) {
-//     char line[12];
-//     FILE *fp=fopen(filename, "r");
-//     int n=0;
-
-//     while(fgets(line, 12, fp)) {
-//         if(line[9]!='\n' && line[9]!='\0') {
-//             printf("wrong format for input\n");
-//             exit(-1);
-//         }
-
-//         for(int i=0; i<9; i++) {
-//             if(line[i]!='0') {
-//                 mat_update(mat, n, line[i]-'0');
-//             	// lat[n].set=1;
-//             	// lat[n].number=line[i]-'0';
-//                 // mat->unset--;
-//             } //else lat_init_unset(lat+n);
-//             n++;
-//         }
-//     }
-
-//     fclose(fp);
-// }
